2a68fab0602796ca829b1b14e4ee8719e96e807a
Jean Orozco <38550524+JeanOrozcoNoguera@users.noreply.github.com>
2022-06-12 17:11:47 -0500

    no sirve este commit

--- src/hooks/useEmployees.js
+++ src/hooks/useEmployees.js
@@ -3,33 +3,33 @@ import axios from "axios"
 
 const useEmployees = () => {
 
-  const postEmployee = async (body) => {
-    const url = 'http://127.0.0.1:8000/api/v1/register-employee'
-    try {
-      const response = await axios.post(url, body)
-      return true
-    } catch (error) {
-      console.log(error)
-      return false
+    const postEmployee = async(body) => {
+        const url = 'http://127.0.0.1:8000/api/v1/register-employee'
+        try {
+            const response = await axios.post(url, body)
+            return true
+        } catch (error) {
+            console.log(error)
+            return false
+        }
     }
-  }
 
-  const getEmployees = async () => {
-    const url = 'http://127.0.0.1:8000/api/v1/list-employees'
-    try {
-      const response = await axios.get(url)
-      console.log(response)
-      return response.data ? response.data : false
-    } catch (error) {
-      console.log(error)
-      return false
+    const getEmployees = async() => {
+        const url = 'http://127.0.0.1:8000/api/v1/list-employees'
+        try {
+            const response = await axios.get(url)
+            console.log(response)
+            return response.data ? response.data : false
+        } catch (error) {
+            console.log(error)
+            return false
+        }
     }
-  }
 
-  return {
-    postEmployee,
-    getEmployees
-  }
+    return {
+        postEmployee,
+        getEmployees
+    }
 }
 
 export default useEmployees
